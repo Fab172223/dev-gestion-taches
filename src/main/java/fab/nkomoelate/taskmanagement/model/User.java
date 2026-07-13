c991db825dd9e0dbd403a791466d72439aa211c6
Fab172223 <fablene@yahoo.fr>
2026-07-13 15:31:42 +0200

    update model for user and project

--- src/main/java/fab/nkomoelate/taskmanagement/model/User.java
+++ src/main/java/fab/nkomoelate/taskmanagement/model/User.java
@@ -1,9 +1,11 @@
 package fab.nkomoelate.taskmanagement.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import lombok.*;
 
 import java.util.List;
+import jakarta.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -18,8 +20,12 @@ public class User {
     private Long id;
     private String lastName;
     private String  firstName;
+
     @Column(nullable = false, unique = true)
+    @NotNull(message="l'email est obligatoire")
+    @Email(message="Format d'email invalide")
     private String email;
+
     @OneToMany(mappedBy = "assignedUser")
     private List<Task> tasks;
 }
