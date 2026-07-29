92d56bea4b53c74ec1b416619a140734bb85752b
Fab172223 <fablene@yahoo.fr>
2026-07-30 00:43:23 +0200

    feat: add password field to CreateUserRequest

--- src/main/java/fab/nkomoelate/taskmanagement/controllers/UserController.java
+++ src/main/java/fab/nkomoelate/taskmanagement/controllers/UserController.java
@@ -6,6 +6,7 @@ import fab.nkomoelate.taskmanagement.services.UserService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +26,10 @@ public class UserController {
                                String  firstName,
                                @NotBlank(message="l'email est obligatoire")
                                @Email(message="Format d'email invalide")
-                               String email){}
+                               String email,
+                               @NotBlank(message = "Le mot de passe est obligatoire")
+                               @Size(min = 8, message = "le mot de passe doit contenir au moins 8 caractères")
+                               String password){}
 
     @PostMapping
     public ResponseEntity<User> addUser(@Valid @RequestBody CreateUserRequest user){
