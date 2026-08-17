7e1332aad82772f12ab3f81aeb382a926b72534e
Fab172223 <fablene@yahoo.fr>
2026-08-17 15:44:24 +0200

    feat: add password field to User model and findByEmail to UserRepository

--- src/main/java/fab/nkomoelate/taskmanagement/model/User.java
+++ src/main/java/fab/nkomoelate/taskmanagement/model/User.java
@@ -4,8 +4,12 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.*;
 
+import java.util.Collection;
 import java.util.List;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Getter
 @Setter
@@ -14,7 +18,7 @@ import jakarta.validation.constraints.NotNull;
 @Builder
 @Entity
 @Table(name="users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -31,4 +35,34 @@ public class User {
     private String password;
     @OneToMany(mappedBy = "assignedUser")
     private List<Task> tasks;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
--- src/main/java/fab/nkomoelate/taskmanagement/repository/UserRepository.java
+++ src/main/java/fab/nkomoelate/taskmanagement/repository/UserRepository.java
@@ -2,8 +2,13 @@ package fab.nkomoelate.taskmanagement.repository;
 
 import fab.nkomoelate.taskmanagement.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
 }
